eab1d17785cdbe9eca136c510a9ea26690d36253
Zuko <365412149@qq.com>
2022-12-28 19:01:39 +0100

    finished work

--- model.py
+++ model.py
@@ -23,9 +23,15 @@ class MyModel(nn.Module):
             nn.Conv2d(32, 64, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.MaxPool2d(2, 2),
             #64x56x56
             nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(2, 2),
@@ -34,9 +40,15 @@ class MyModel(nn.Module):
             nn.Conv2d(128, 256, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
             nn.MaxPool2d(2, 2),
             #256x14x14
             nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(512),
             nn.MaxPool2d(2, 2),
@@ -45,10 +57,11 @@ class MyModel(nn.Module):
             nn.Flatten(),
             nn.Linear(512 * 7 * 7, 512),
             nn.Dropout(dropout),
-            
-            
             nn.Linear(512, 128),
-            nn.Linear(128, num_classes)
+            nn.Linear(128, 84),
+            nn.Linear(84, num_classes)
+
+
 
             )
 
